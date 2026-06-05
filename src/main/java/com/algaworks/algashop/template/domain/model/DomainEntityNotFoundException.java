e2748c9352d59cd63129ae74d19ca93e3bbd0785
Breno Pedro <brenopedro14@hotmail.com>
2026-06-05 18:09:05 -0300

    feat: criado projeto template

---
+++ src/main/java/com/algaworks/algashop/template/domain/model/DomainEntityNotFoundException.java
@@ -0,0 +1,14 @@
+package com.algaworks.algashop.template.domain.model;
+
+public class DomainEntityNotFoundException extends RuntimeException {
+
+    public DomainEntityNotFoundException() {}
+
+    public DomainEntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public DomainEntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
